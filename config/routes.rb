a1067a933c3025505fae0e885d78af2bb355adb7
Whoistolu <digittolu25@gmail.com>
2022-01-12 17:41:57 +0100

    Fix linter erro

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
   root to: 'users#index'
   # Defines the root path route ("/")
