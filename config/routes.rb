6de98d702f57bb034990bd49bde3cc612112804c
Whoistolu <digittolu25@gmail.com>
2022-01-14 10:04:40 +0100

    Set up tests on the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'posts/index'
+  get 'posts/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
