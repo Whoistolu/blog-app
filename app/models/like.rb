1c049a1a325ccc8cbd48affd33afd1017cc2974b
Whoistolu <digittolu25@gmail.com>
2022-01-19 00:29:50 +0100

    Add a method that updates the likes counter for a post

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_save :update_comments_counter
+
+  private
+
   def update_likes_counter
-    
+    post.increment!(:likes_counter)
   end
 end
