128ca7210492d8e56e36caef311327f74f1357b1
Whoistolu <digittolu25@gmail.com>
2022-01-19 00:07:53 +0100

    Add a method that updates the posts counter for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,12 @@ class Post < ApplicationRecord
   def recent_5_comments
     comment.order('created_at Desc').limit(5)
   end
+
+  after_save :update_posts_counter
+
+  private
+
+  def update_posts_counter
+    user.increment!(:posts_counter)
+  end
 end
