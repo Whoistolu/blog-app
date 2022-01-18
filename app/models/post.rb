1b9f3779bceaaeb1fc8b53bb8d0929bdd0fe6c9a
Whoistolu <digittolu25@gmail.com>
2022-01-19 00:05:25 +0100

    Add a method method which returns the 5 most recent comments for a given post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
   belongs_to :user
+
+  def recent_5_comments
+    comment.order('created_at Desc').limit(5)
+  end
 end
