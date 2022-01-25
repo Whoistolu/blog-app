aebb5245c35235f86c01466f7855678b3a2b5596
Whoistolu <digittolu25@gmail.com>
2022-01-25 22:36:31 +0100

    Adding views to the controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name: "Ben", photo: "url", bio: "Hey my name is ben, I'm dev", posts_counter: 5)
-User.create(name: "Taliah", photo: "url", bio: "Hey my name is Taliah", posts_counter: 0)
-User.create(name: "Omar", photo: "url", bio: "Hey my name is Omar", posts_counter: 3)
-User.create(name: "Deen", photo: "url", bio: "Hey my name is Deen", posts_counter: 5)
+user1 = User.create(name: "Ben", photo: "url", bio: "Hey my name is ben, I'm dev", posts_counter: 0)
+user2 = User.create(name: "Taliah", photo: "url", bio: "Hey my name is Taliah", posts_counter: 0)
+user3 = User.create(name: "Omar", photo: "url", bio: "Hey my name is Omar", posts_counter: 0)
+user4 = User.create(name: "Deen", photo: "url", bio: "Hey my name is Deen", posts_counter: 0)
+
+post1 = user1.posts.create!(user_id: 1, title: 'Cats and Dogs', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', comments_counter: 0, likes_counter: 0)
+post2 = user1.posts.create!(user_id: 1, title: 'Food', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', comments_counter: 0, likes_counter: 0)
+post3 = user1.posts.create!(user_id: 1, title: 'Cars', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', comments_counter: 0, likes_counter: 0)
