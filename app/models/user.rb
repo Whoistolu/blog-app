bf6e0d94696407050e9a28d995a5d2c20c005ac7
Whoistolu <digittolu25@gmail.com>
2022-01-25 16:21:46 +0100

    Create model class for all entities and set up associations between models

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
-
-  def recent_3_posts
-    Post.last(3)
-  end
-end
