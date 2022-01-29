e69de5725bd71e354f3208e25c8f3ddbe87fbfe8
Whoistolu <digittolu25@gmail.com>
2022-01-29 22:58:55 +0100

    Implement negative test case as recommended by the previos reviwer

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe Comment, type: :model do
 
     before { subject.save }
 
+    it 'check if comments text matches' do
+        expect(subject.text).to eq('test post 1')
+      end
+
+      it 'checks that after adding the comment counter should not be same' do
+        prev = Post.find(1).comments_counter
+        subject.update_comments_counter
+        expect(Post.find(1).comments_counter).to_not eq(prev)
+      end
+
     it 'check if it increases the comments' do
       prev = Post.find(1).comments_counter
       subject.update_comments_counter
