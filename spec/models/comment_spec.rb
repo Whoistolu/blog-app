612c506910fe148e88eeaf90513154e5c875bc28
Whoistolu <digittolu25@gmail.com>
2022-01-29 23:00:51 +0100

    Fix linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Comment, type: :model do
 
     it 'check if comments text matches' do
         expect(subject.text).to eq('test post 1')
-      end
+    end
 
-      it 'checks that after adding the comment counter should not be same' do
+    it 'checks that after adding the comment counter should not be same' do
         prev = Post.find(1).comments_counter
         subject.update_comments_counter
         expect(Post.find(1).comments_counter).to_not eq(prev)
-      end
+    end
 
     it 'check if it increases the comments' do
       prev = Post.find(1).comments_counter
