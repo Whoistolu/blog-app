6c028d57ccb69dae2f4b30b57d2a8cfc6808934d
Whoistolu <digittolu25@gmail.com>
2022-01-29 19:22:37 +0100

    Add unit testing

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Comment model' do
+    subject { Comment.new(text: 'test post 1', user_id: 1, post_id: 1) }
+
+    before { subject.save }
+
+    it 'check if it increases the comments' do
+      prev = Post.find(1).comments_counter
+      subject.update_comments_counter
+      expect(Post.find(1).comments_counter).to eq(prev + 1)
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Post Model' do
+    subject { Post.create(title: 'Title test', text: 'text', user_id: 1) }
+    before { subject.save }
+
+    it 'checks if subject is invalid if there is no title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'tests if title more than 250chs to be invalid' do
+      subject.title = 'more than 250 more than 250 more than 250 more
+      than 250 more than 250 more than 250 more than 250 more than 250 more than 250 more than 250 more than 250 more
+      than 250 more than 250 more than 250 more than 250 more than 250 more than 250 more than 250 '
+      expect(subject).to_not be_valid
+    end
+  end
+  describe 'Post Model methods' do
+    before do
+      6.times do |number|
+        subject.id = 1
+        Comment.create(text: "test comment #{number}", user_id: 1, post: subject)
+      end
+    end
+
+    it 'loads only the first five comments' do
+      expect(subject.recent_5_comments.length).to eq(5)
+    end
+
+    it 'check if it increases the posts' do
+      subject.user = User.find(1)
+      prev = User.find(1).posts_counter
+      subject.update_posts_counter
+      expect(User.find(1).posts_counter).to eq(prev + 1)
+    end
+  end
 end
