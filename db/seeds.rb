f7e171646b4d5c481ed4d4746a3c2456370655da
Moises <mhdezcoronado@gmail.com>
2022-02-01 18:48:03 +0100

    Fix linters errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,8 +6,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-
 
 # user1 = User.create(name: "Ben", photo: "url", bio: "Hey my name is ben, I'm dev", posts_counter: 0)
 # user2 = User.create(name: "Taliah", photo: "url", bio: "Hey my name is Taliah", posts_counter: 0)
