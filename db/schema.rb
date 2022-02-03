b684ad7202a9fb90cf720e1be461f8e81e316ea0
Whoistolu <digittolu25@gmail.com>
2022-02-03 16:06:37 +0100

    Install cancan, add role column to the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_141740) do
+ActiveRecord::Schema.define(version: 2022_02_03_150352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_141740) do
     t.datetime "confirmation_sent_at", precision: 6
     t.string "unconfirmed_email"
     t.integer "posts_counter", default: 0, null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
