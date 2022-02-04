bdff7c892aa1f05651f2ce9182d2c16edbc2882b
Whoistolu <digittolu25@gmail.com>
2022-02-04 18:59:33 +0100

    Create test for user index page

---
+++ spec/views/user_index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Login', type: :system do
+  describe 'User' do
+    before(:each) do
+      @User1 = User.create! name: 'Chaw', password: '123456', email: 'chaw@gmail.com', confirmed_at: Time.now
+      visit root_path
+      fill_in 'Email', with: 'chaw@gmail.com'
+      fill_in 'Password', with: '123456'
+      click_button 'Log in'
+      visit root_path
+    end
+    
+    it 'shows the username' do
+      expect(page).to have_content('Chaw')
+    end
+
+    it 'shows photo' do
+      image = page.all('img')
+      expect(image.size).to eql(1)
+    end
+
+    
+    it 'shows number of posts for each user' do
+      expect(page).to have_content('Number of posts: 0')
+    end
+
+    it 'show users page when clicked' do
+      expect(page).to have_content('Number of posts: 0')
+      click_on 'Chaw'
+      expect(page).to have_current_path user_path(@User1)
+    end
+  end
+end 
