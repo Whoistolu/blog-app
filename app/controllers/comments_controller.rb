335b7ca4e5adc5388d2dacc2138d1488b3a718c4
Moises <mhdezcoronado@gmail.com>
2022-02-04 11:00:43 +0100

    Fix adding comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:commnet_id])
+    @comment.destroy!
+    flash[:success] = 'You have deleted this post!'
+    redirect_to user_path(current_user.id)
+  end
+
   private
 
   def comment_params
