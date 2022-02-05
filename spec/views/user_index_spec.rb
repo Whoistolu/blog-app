de2eb3dd2c7357272c597b95b174b287974920ac
Whoistolu <digittolu25@gmail.com>
2022-02-05 11:13:25 +0100

    Add one more user and tested the visibility of their usernames

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Login', type: :feature do
   describe 'User' do
     before(:each) do
       @user1 = User.create! name: 'Chaw', password: '123456', email: 'chaw@gmail.com', confirmed_at: Time.now
+      @user2 = User.create! name: 'Moises', password: '123456', email: 'moises@gmail.com', confirmed_at: Time.now
       visit root_path
       fill_in 'Email', with: 'chaw@gmail.com'
       fill_in 'Password', with: '123456'
@@ -13,13 +14,14 @@ RSpec.describe 'Login', type: :feature do
       visit root_path
     end
 
-    it 'shows the username' do
+    it 'shows the username of other users' do
       expect(page).to have_content('Chaw')
+      expect(page).to have_content('Moises')
     end
 
     it 'shows photo' do
       image = page.all('img')
-      expect(image.size).to eql(1)
+      expect(image.size).to eql(2)
     end
 
     it 'shows number of posts for each user' do
@@ -30,6 +32,7 @@ RSpec.describe 'Login', type: :feature do
       expect(page).to have_content('Number of posts: 0')
       click_on 'Chaw'
       expect(page).to have_current_path user_path(@user1)
+      expect(page).to have_no_content('Moises')
     end
   end
 end
